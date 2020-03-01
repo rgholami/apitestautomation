5a8329552148d82ab1ceaa73536ab4d95f1c74ca
rgholami <rgholami@comcast.net>
2020-03-01 14:22:08 -0800

    Rexa imported from my Github- Tracking Vehicle

--- src/main/java/CustomerClass.java
+++ src/main/java/CustomerClass.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
-
 public class CustomerClass {
+
+
+
+    // 1- create Getter/Setter - "Cntrol + N" HIGHLIGHT ARRTIBUTES AND HIT contl + N to create gettter.setter methods
+    // 2-  to comment code-block, highlig the code block and use Command"  + "/"
+    // 2B-b to uncomment a block of code, "Command + "/
+    // 3- to refactoring, highlight attribute, use tab "Refactor then change/refactor to a different name,
+    // Getter/Setter for the changed apptibute - refactors
+//  4- Declaration /usage - click on "age , for example, click "command"  key, it shows all useage of attribut
+    // attribute, its gettter/Setter
+    // 50 Artifacts = to create jar, you need to create artifact,
+    // 6- Crerate jar file from Artifact -- go project structure + Command + ";" --same Jar from main
+
+
+
+    String firstName ;
+    String lastName;
+    int age;
+    String address;
+
+    public String getFirstName () {
+        return firstName;
+    }
+
+    public void setFirstName (String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName () {
+        return lastName;
+    }
+
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge () {
+        return age;
+    }
+
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    public String getAddress () {
+        return address;
+    }
+
+    public void setAddress (String address) {
+        this.address = address;
+    }
 }
--- src/main/java/DemoMain.java
+++ src/main/java/DemoMain.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class DemoMain {
+
+
+    public static void main (String args[]) {
+
+        String teststring = "hello World ";
+
+//    public void setTeststring (String teststring) {this.teststring = teststring;}
+
+
+        System.out.println (" output :" + teststring);
+
+        String str = "Hello World";
+        String anotherString = "hello world";
+        Object objStr = str;
+
+        /* compare two strings, case sensitive */
+        System.out.println("test-1 :" +str.compareTo(anotherString) );
+        /* compare two strings, ignores character case  */
+        System.out.println("test-2  :" + str.compareToIgnoreCase(anotherString) );
+        /* compare string with object */
+        System.out.println("Test-3 comapre string to object :  " + str.compareTo((String) objStr) );
+
+        CustomerClass  customerc = new CustomerClass() ;
+
+        customerc.setFirstName ("Reza");
+
+        System.out.print ("First name sent : "+ customerc.getFirstName ());
+
+    }
+
 }
